0ab01d43f1bda33a2e60b7137767cc215b0d65c9
Sam Krol <samkrol97@gmail.com>
2020-12-14 13:59:32 +0000

    Smooth scrolling design
    First time using styled components and BrowserRouter.
    
    Designed with phones and tablets in mind.

---
+++ src/components/Footer/index.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { FaFacebook, FaInstagram, FaYoutube, FaTwitter } from "react-icons/fa";
+import {
+  FooterContainer,
+  FooterWrap,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  WebisteRights,
+  SocialIcons,
+  SocialIconsLink,
+} from "./FooterElements";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to="/signin">How It Works</FooterLink>
+              <FooterLink to="/signin">Testimonials</FooterLink>
+              <FooterLink to="/signin">Careers</FooterLink>
+              <FooterLink to="/signin">Investors</FooterLink>
+              <FooterLink to="/signin">Terms of Service</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Social Media</FooterLinkTitle>
+              <FooterLink to="/signin">Instagram</FooterLink>
+              <FooterLink to="/signin">Facebook</FooterLink>
+              <FooterLink to="/signin">YouTube</FooterLink>
+              <FooterLink to="/signin">Twitter</FooterLink>
+              <FooterLink to="/signin">Google</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>Contact Us</FooterLinkTitle>
+              <FooterLink to="/signin">Contact</FooterLink>
+              <FooterLink to="/signin">Support</FooterLink>
+              <FooterLink to="/signin">FAQ</FooterLink>
+              <FooterLink to="/signin">Sponsorships</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Submissions</FooterLinkTitle>
+              <FooterLink to="/signin">Submit Media</FooterLink>
+              <FooterLink to="/signin">Ambassadors</FooterLink>
+              <FooterLink to="/signin">Agency</FooterLink>
+              <FooterLink to="/signin">Influencer</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="/">Moolah</SocialLogo>
+            <WebisteRights>
+              Moolah © {new Date().getFullYear()}All rights reserved.
+            </WebisteRights>
+            <SocialIcons>
+              <SocialIconsLink
+                href="//www.facebook.com"
+                target="_blank"
+                aria-label="Facebook"
+              >
+                <FaFacebook />
+              </SocialIconsLink>
+              <SocialIconsLink
+                href="//www.instagram.com"
+                target="_blank"
+                aria-label="Instagram"
+              >
+                <FaInstagram />
+              </SocialIconsLink>
+              <SocialIconsLink
+                href="//www.youtube.com"
+                target="_blank"
+                aria-label="Youtube"
+              >
+                <FaYoutube />
+              </SocialIconsLink>
+              <SocialIconsLink
+                href="//www.twitter.com"
+                target="_blank"
+                aria-label="Twitter"
+              >
+                <FaTwitter />
+              </SocialIconsLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,53 @@
+export const HomeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimted transactions without getting charged any fees.",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: "../../images/svg-2.svg",
+  alt: "World",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const HomeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Unlimited Access",
+  headline: "Connect whenever you need!",
+  description:
+    "Using our service, you are able to access your accounts anywhere accross the globe",
+  buttonLabel: "Connect with us",
+  imgStart: true,
+  img: "../../images/svg-3.svg",
+  alt: "Money",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const HomeObjThree = {
+  id: "signup",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Join our Team",
+  headline: "Creating an account is really easy",
+  description:
+    "Get everything set up and ready within 10 minutes. Just add your information and you're ready to go.",
+  buttonLabel: "Start now",
+  imgStart: false,
+  img: "../../images/svg-1.svg",
+  alt: "World",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
